4bb1382d243b1d6dc0d246c8977da99baaa66314
Omanju <omanju@centos.local>
2012-11-21 01:18:09 +0900

    途中だけどそろそろ寝るので続きはまたMacbookAirで！

--- main.cpp
+++ main.cpp
@@ -18,20 +18,65 @@ std::ifstream fin;
 
 KeyWord tblKeyWord[] = 
 {
-    KeyWord("if", If),
-    KeyWord("else", Else),
-    KeyWord("if", If),
-    KeyWord("if", If),
-    KeyWord("if", If),
-    KeyWord("if", If),
-    KeyWord("if", If),
-    KeyWord("if", If),
-    KeyWord("if", If),
+    KeyWord("if",    If),     KeyWord("else",  Else),
+    KeyWord("end",   End),    KeyWord("print", Print),
+    KeyWord("(",     Lparen), KeyWord(")",     Rparen),
+    KeyWord("+",     Plus),   KeyWord("-",     Minus),
+    KeyWord("*",     Multi),  KeyWord("/",     Divi),
+    KeyWord("=",     Assign), KeyWord(",",     Comma),
+    KeyWord("==",    Equal),  KeyWord("!=",    NotEqual),
+    KeyWord("<",     Less),   KeyWord("<=",    LessEq),
+    KeyWord(">",     Great),  KeyWord(">=",    GreatEq),
+    KeyWord("",      END_List),
 };
 
 int main(int argc, char * argv[])
 {
-    std::cout << "Hello! Token!" << std::endl;
+    if(argc == 1)
+    {
+        std::exit(1);
+    }
+
+    fin.open(argv[1]);
+    if(!fin)
+    {
+        std::exit(1);
+    }
+
+    std::cout << "text   kind intValue" << std::endl;
+    initCharType();
+
+    for(token = nextToken(); token.type() != EofTkn; token = nextToken())
+    {
+        std::cout << std::left << std::setw(10) << token.text()
+                  << std::right << std::setw(3) << token.type()
+                  << " " << token.intValue() << std::endl;
+    }
     
     return 0;
+}
+
+Token nextToken()
+{
+    TokenType type;
+    int ch0, num = 0;
+    std::string txt = "";
+
+    // 空行読み捨て
+    static int ch = ' ';
+    while(std::isspace(ch))
+    {
+        ch = nextChar();
+    }
+
+    if(ch == EOF)
+    {
+        return Token(EofTkn, txt);
+    }
+
+    switch(ctype[ch])
+    {
+        case Letter:
+    }
+    
 }
